b3a14459634f3c0ad89821d880558c238881b22f
Yashika Khattri <88974920+yashika232@users.noreply.github.com>
2025-06-04 23:40:16 +0530

    Update gemini_api.py

--- utils/gemini_api.py
+++ utils/gemini_api.py
@@ -4,9 +4,7 @@ from google.generativeai import types
 # Set your Gemini API key
 genai.configure(api_key="AIzaSyBFW9bj0zNhAveWUWe7MFxUrUGSIJdbQMg")
 
-system_prompt = """You are a dream analyst trained in Calvin Hall’s Cognitive Theory of Dreams.
-Your role is to interpret dreams by focusing on how they reflect the dreamer’s thoughts, personal concerns, and worldview.
-
+system_prompt = """You are a dream analyst trained in Calvin Hall’s Cognitive Theory of Dreams. Your role is to interpret dreams by focusing on how they reflect the dreamer’s thoughts, personal concerns, and worldview.
 Your analysis should include:
 1. Thought Reflection:
    - What dominant thoughts or concerns does the dream reveal?
@@ -20,8 +18,8 @@ Your analysis should include:
 4. Cognitive Resolution:
    - Suggest what internal dilemmas the dream may be processing.
    - Provide insights to help the dreamer understand how the dream fits into their current mental and emotional state.
+Focus on rational, structured interpretation. Avoid archetypes, supernatural meanings, or overly symbolic metaphors. Your goal is to help the user reflect cognitively and emotionally on their dream in a meaningful, modern psychological context.summarize in sharp 3 points. Provide a insightfull conclusion at end."""
 
-Summarize in sharp 3 points. Provide an insightful conclusion at the end."""
 
 def generate_interpretation(user_dream, matched_symbols):
     combined_prompt = f"""
@@ -33,7 +31,7 @@ Dream Description:
 Symbols from dream dictionary:
 {matched_symbols}
 
-Now provide a detailed interpretation using Calvin Hall’s Cognitive Theory by identifying the dominant emotion, core thought or concern, symbolic themes, focus of the dream (self or others), any cognitive conflict, the personal concern category (e.g., relationships or identity), and the overall cognitive theme.
+Now provide a detailed interpretation using  Calvin Hall’s Cognitive Theory by identifying the dominant emotion, core thought or concern, symbolic themes, focus of the dream (self or others), any cognitive conflict, the personal concern category (e.g., relationships or identity), and the overall cognitive theme.
 """
     model = genai.GenerativeModel("gemini-2.0-flash")
     response = model.generate_content(
